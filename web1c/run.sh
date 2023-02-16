f9f95d9ce64dbacc7139525f291e7b7a586d3c67
igp <sergey@nqnet.org>
2023-02-16 16:34:11 -0500

    WEB1C: web-sharing config handling

--- web1c/run.sh
+++ web1c/run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-docker run --rm -it  \
+docker run -d  \
+  --restart always \
   -h web1c \
   -p 8080:80 \
   --name web1c \
-  -v /home/nquest/1cwww:/var/www \
-  web1c:latest \
-  /bin/bash
+  --net internal \
+  -v www-data:/var/www \
+  web1c:latest
